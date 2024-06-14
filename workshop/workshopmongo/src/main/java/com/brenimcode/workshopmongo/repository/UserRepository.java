a71a3068d9b5c4c605a28e511c6c58d927e788ff
BrenoCode <breno.cavalcante@ufu.br>
2024-06-14 15:19:22 -0300

    Connecting to MONGODB with repository and service

---
+++ workshop/workshopmongo/src/main/java/com/brenimcode/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.brenimcode.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.brenimcode.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+	
+	
+}
